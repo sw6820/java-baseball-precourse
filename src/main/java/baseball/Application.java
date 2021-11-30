fde791d5d91c77f163be23cdfb9d4ff987ad6e6b
sw6820 <52646313+sw6820@users.noreply.github.com>
2021-11-30 22:53:27 +0900

    Add files via upload

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,9 +2,36 @@ package baseball;
 
 import java.util.Scanner;
 
+import utils.Ball;
+import utils.Console;
+import utils.Game;
+
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
+        run(scanner);
+    }
+
+    private static void run(Scanner scanner){
+        Ball ball = new Ball();
+        Game game = new Game();
+        ball.generateBall();
+        game.startGame();
+
+        do {
+            Console.consolePrint(Console.INPUT_NUMBER);
+            String input = scanner.nextLine();
+            String strikeBall = game.game(input, ball.balls);
+            System.out.println(strikeBall);
+        } while(game.isContinue());
+    }
+
+    public void gameStart(Scanner scanner) {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        if (scanner.nextLine().equals("1")) {
+            run(scanner);
+        }
     }
 }
