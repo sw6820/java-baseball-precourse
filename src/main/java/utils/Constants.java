ccf32002cd4d40bca180b96a02033299a9d1200c
sw6820 <52646313+sw6820@users.noreply.github.com>
2021-11-30 23:10:36 +0900

    Add files via upload

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Constants {
+    public static final int MIN_BALL = 1;
+    public static final int MAX_BALL = 9;
+    public static final int BALL_SIZE = 3;
+
+    public static final String NOTHING = "낫싱";
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+}
